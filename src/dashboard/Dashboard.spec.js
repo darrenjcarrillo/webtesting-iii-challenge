cadbe469ceb693ca427ec5969a2d0a805d772a23
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-10-15 08:02:17 -0700

    Done with Dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,43 @@
 // Test away
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import Dashboard from "./Dashboard";
+import Controls from "../Controls/Controls";
+
+test("Dashboard renders correctly", () => {
+  render(<Dashboard />);
+});
+
+test("Gate is default to unlocked", () => {
+  const unlockedGate = jest.fn();
+
+  const { getByText } = render(<Controls locked={false} closed={false} />);
+
+  fireEvent.click(getByText(/close gate/i));
+  expect(unlockedGate).toHaveBeenCalledTimes(0);
+});
+
+test("cant be closed or opened if its locked", () => {
+  const superLockedGate = jest.fn();
+
+  const { getByText } = render(
+    <Controls toggleLocked={superLockedGate} locked={true} closed={true} />
+  );
+
+  fireEvent.click(getByText(/unlock gate/i));
+  expect(superLockedGate).toHaveBeenCalledTimes(1);
+  expect(superLockedGate).toBeTruthy();
+});
+
+test("cant be closed or opened if its locked", () => {
+  const lockedGate = jest.fn();
+
+  const { getByText } = render(
+    <Controls toggleClosed={lockedGate} locked={false} closed={true} />
+  );
+
+  fireEvent.click(getByText(/open gate/i));
+  expect(lockedGate).toHaveBeenCalledTimes(1);
+  expect(lockedGate).toBeTruthy();
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,39 @@
-// Test away!
+// Test away
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import Controls from "../Controls/Controls";
+import Display from "./Display";
+
+test("Controls renders correctly", () => {
+  render(<Controls />);
+});
+
+// test("Display if gate is open/closed and if it is locked/unlocked", () => {
+//   const displayOpenGate = jest.fn();
+
+//   const { getByText } = render(<Controls locked={false} locked={false} />);
+
+//   fireEvent.click(getByText(/open/i));
+//   expect(displayOpenGate).toHaveBeenCalledTimes(1);
+// });
+// test("Display if gate is open/closed and if it is locked/unlocked", () => {
+//   const displayOpenGate = jest.fn();
+
+//   const { getByText } = render(<Controls locked={false} closed={false} />);
+
+//   fireEvent.click(getByClass(/open/i));
+//   expect(displayOpenGate).toHaveBeenCalledTimes(0);
+// });
+
+test("Displays 'Closed' if the `closed` prop is `true`", () => {
+  const lockedGate = jest.fn();
+
+  const { getByText } = render(
+    <Display toggleClosed={lockedGate} locked={false} closed={true} />
+  );
+
+  fireEvent.click(getByText(/open/i));
+  expect(lockedGate).toHaveBeenCalledTimes(1);
+  expect(lockedGate).toBeTruthy();
+});
